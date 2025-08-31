b763af8f9316b4679c1f21a1f15c3900ee36e4ea
Gaurav0developer0tester0creator <gauravpimpalkar2207@gmail.com>
2025-08-31 05:32:24 +0530

    Deployed updated site with logo and favicon

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  distDir: 'out', // 👈 force Next.js to use /out instead of .next
-  images: { unoptimized: true },
 };
 
 module.exports = nextConfig;
